a9d1731d07ef7908d4859de7bc4eedf8164e9373
Craig5117 <craig.bennett5117@gmail.com>
2021-03-01 14:01:04 -0500

    set up tests for Nav

--- src/components/About/__tests__/index.test.js
+++ src/components/About/__tests__/index.test.js
@@ -9,7 +9,7 @@ describe('About component', () => {
         render(<About />);
     });
     // Second Test
-    it('matches snapshot DOM nore structure', () => {
+    it('matches snapshot DOM node structure', () => {
         // render About
         const { asFragment } = render(<About />);
         expect(asFragment()).toMatchSnapshot();
