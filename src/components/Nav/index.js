7c31406bccdbbe0a78814e61910ef53cb99c228a
Craig5117 <craig.bennett5117@gmail.com>
2021-03-02 17:38:49 -0500

    useEffect to change doc title is working now

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { capitalizeFirstLetter } from "../../utils/helpers";
 
 
@@ -9,9 +9,9 @@ function Nav(props) {
     currentCategory,
   } = props;
 
-  // useEffect(() => {
-  //   document.title = capitalizeFirstLetter(currentCategory.name);
-  // }, [currentCategory]);
+  useEffect(() => {
+    document.title = capitalizeFirstLetter(currentCategory.name);
+  }, [currentCategory]);
 
   return (
     <header className="flex-row px-1">
