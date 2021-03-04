df6a7596546f6b06f87f9a76aa38c5b6b3731a90
Craig5117 <craig.bennett5117@gmail.com>
2021-03-04 10:41:06 -0500

    set up conditional rendering of modal

--- src/components/PhotoList/index.js
+++ src/components/PhotoList/index.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import Modal from '../Modal';
 
 function PhotoList(props) {
   const { category } = props;
@@ -120,15 +121,22 @@ function PhotoList(props) {
   ]);
 
   const currentPhotos = photos.filter((photo) => photo.category === category);
-  
+  const [currentPhoto, setCurrentPhoto] = useState();
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const toggleModal = (image, i) => {
+    setCurrentPhoto({...image, index: i});
+    setIsModalOpen(!isModalOpen);
+  }
   return (
     <div>
+      {isModalOpen && <Modal currentPhoto={currentPhoto} onClose={toggleModal}/>}
       <div>
         {currentPhotos.map((image, i) => ( 
           <img
             src={require(`../../assets/small/${category}/${i}.jpg`).default}
             alt={image.name}
             className="img-thumbnail mx-1"
+            onClick={() => toggleModal(image, i)}
             key={image.name}
           />
         ))}
